425a13dfbec87e3f854ca0e22aa92298388aa01a
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-04-23 02:33:28 +0100

    Added navbar to signin and signup

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -4,11 +4,11 @@ import {loginUser} from '../models'
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import NavbarComponent from '../components/Navbar';
 
 export default function SignIn() {
   let history = useHistory()
   const [validated, setValidated] = useState(false);
-  let emailError, passwordError
 
   if (localStorage.getItem('userId')) {
   history.push('/') 
@@ -16,15 +16,15 @@ export default function SignIn() {
   const email = useRef('')
   const password = useRef('')
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     const body = {
       email: email.current.value,
       password: password.current.value
     }
     // Handle login logic
     if (!body.email || !body.password) {
-      alert('You need to input an email and password')
+      setValidated(true)
     } else {
       const user = await loginUser(body.email, body.password)
       console.log(user)
@@ -38,32 +38,37 @@ export default function SignIn() {
     }
   }
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit} style={{margin: 'auto'}}>
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            ref={email}
-            type="email"
-            placeholder="Email"
-          />
-          <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom02">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            required
-            ref={password}
-            type="password"
-            placeholder="Password"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Button type="submit">SignUp</Button>
-    </Form>
+    <>
+      <NavbarComponent />
+      <Form noValidate validated={validated} onSubmit={handleSubmit} style={{margin: 'auto'}}>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom01">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              required
+              ref={email}
+              type="email"
+              placeholder="Email"
+            />
+            <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
+            <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom02">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              ref={password}
+              type="password"
+              placeholder="Password"
+            />
+            <Form.Control.Feedback type="invalid">Please provide a password.</Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Button type="submit">Sign In</Button>
+      </Form>
+    </>
   )
 }
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -4,6 +4,7 @@ import {useHistory} from 'react-router-dom'
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import NavbarComponent from '../components/Navbar';
 
 export default function SignIn() {
   const history = useHistory()
@@ -35,8 +36,8 @@ export default function SignIn() {
         history.push('/')
         alert('Account created sucessfully, signing you in...')
       }
-    } else if (!body.firstName || !body.email || !body.lastName || !body.password) {
-      alert('You didn\'t pass any value')
+    } else if (!body.firstName || !body.email || !body.lastName || !body.password || (body.password !== confirm_password.current.value)) {
+      setValidated(true)
     } else {
       alert('Password and confirm password fields must be equal')
     }
@@ -45,68 +46,73 @@ export default function SignIn() {
   }
   
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
+    <>
+      <NavbarComponent />
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Row>
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              required
+              ref={firstName}
+              type="text"
+              placeholder="First name"
+            />
+            <Form.Control.Feedback type="invalid">Please provide an email.</Form.Control.Feedback>
+            <Form.Control.Feedback>Great name!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              required
+              ref={lastName}
+              type="text"
+              placeholder="Last name"
+            />
+            <Form.Control.Feedback type="invalid">Please provide your last email.</Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              aria-describedby="inputGroupPrepend"
+              required
+              ref={email}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom03">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="........" required ref={password} />
+            <Form.Control.Feedback type="invalid">
+              Please provide a password between 8 and 20.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="validationCustom04">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="......" required ref={confirm_password} />
+            <Form.Control.Feedback type="invalid">
+              Fields do not match.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Group>
+          <Form.Check
             required
-            ref={firstName}
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
           />
-          <Form.Control.Feedback>Great name!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Looks good!</Form.Label>
-          <Form.Control
-            required
-            ref={lastName}
-            type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Email"
-            aria-describedby="inputGroupPrepend"
-            required
-            ref={email}
-          />
-          <Form.Control.Feedback type="invalid">
-            Please choose a valid email.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="........" required ref={password} />
-          <Form.Control.Feedback type="invalid">
-            Please provide a password between 8 and 20.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="6" controlId="validationCustom04">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="......" required ref={confirm_password} />
-          <Form.Control.Feedback type="invalid">
-            Fields do not match.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Form.Group>
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-        />
-      </Form.Group>
-      <Button type="submit">SignUp</Button>
-    </Form>
+        <Button type="submit">SignUp</Button>
+      </Form>
+    </>
   )
 }
