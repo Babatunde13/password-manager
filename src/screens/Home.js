6759565b67a3d1f997d9eed889161eb0bcd5160d
wolz-CODElife <joeladewole3@gmail.com>
2021-05-01 21:30:47 +0100

    Added footer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import NavbarComponent from "../components/Navbar";
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import hero1 from '../assets/illus8.jpg';
@@ -17,7 +18,7 @@ const Home = () => {
         <img src={hero} alt="" className='shadow-lg' style={{border : 'none', borderRadius : '15px', maxWidth : '90%', maxHeight : '75%'}}></img>
         <img src={hero1} alt="" className='h-25 shadow-lg mx-2 ' style={{border : 'none', borderRadius : '15px'}}></img>
       </Container>
-      <p class="w-100 text-center">Built with <FontAwesomeIcon icon={faHeart} className="text-danger" /> by <a target="_blank" href="http://twitter.com/@bkoiki950">Babatunde Koiki</a> and <a target="_blank" href="http://twitter.com/@AdewolzJ">wolz-CODElife</a></p>
+      <p class="navbar fixed-bottom d-block w-100 m-0 text-center" style={{backgroundColor : "#d1e1f0e7"}} >Built with <FontAwesomeIcon icon={faHeart} className="text-danger" /> by <Link target="_blank" to={{ pathname: "https://twitter.com/bkoiki950"}}>Babatunde Koiki</Link> and <Link target="_blank" to={{ pathname: "https://twitter.com/AdewolzJ"}}>wolz-CODElife</Link></p>
     </div>
   )
 }
