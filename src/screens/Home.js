5c1f26a0502416eccf4759f1a922005b1eafaa8b
wolz-CODElife <joeladewole3@gmail.com>
2021-05-01 21:28:23 +0100

    Added footer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,7 @@
 import NavbarComponent from "../components/Navbar";
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import hero1 from '../assets/illus8.jpg';
 // import hero from '../assets/illus7.png';
 import hero from '../assets/illus4.png';
@@ -15,6 +17,7 @@ const Home = () => {
         <img src={hero} alt="" className='shadow-lg' style={{border : 'none', borderRadius : '15px', maxWidth : '90%', maxHeight : '75%'}}></img>
         <img src={hero1} alt="" className='h-25 shadow-lg mx-2 ' style={{border : 'none', borderRadius : '15px'}}></img>
       </Container>
+      <p class="w-100 text-center">Built with <FontAwesomeIcon icon={faHeart} className="text-danger" /> by <a target="_blank" href="http://twitter.com/@bkoiki950">Babatunde Koiki</a> and <a target="_blank" href="http://twitter.com/@AdewolzJ">wolz-CODElife</a></p>
     </div>
   )
 }
