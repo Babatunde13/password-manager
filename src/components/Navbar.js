f0999c1026f0ce6ca83cd335c4cbd929b99702a5
wolz-CODElife <joeladewole3@gmail.com>
2021-05-01 15:35:41 +0100

    Rearrange user icon on register

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const NavbarComponent = (props) => {
   }
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="mb-5">
     {/* <Navbar bg="light" expand="lg" style={{ width: 400 }}> */}
       <Navbar.Brand onClick={() => {window.location.assign("/")}} style={{cursor : 'pointer'}}><img src={favicon} alt="" style={{width : '40px', height :  '40px'}}></img> Password Manager</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,16 @@
 import NavbarComponent from "../components/Navbar";
 import Container from 'react-bootstrap/Container';
 import hero1 from '../assets/illus8.jpg';
-import hero from '../assets/illus3.png';
-// import hero from '../assets/illus4.png';
 // import hero from '../assets/illus7.png';
+import hero from '../assets/illus4.png';
+// import hero from '../assets/illus3.png';
 
 
 const Home = () => {
   return (
     <div>
       <NavbarComponent />
-      <Container style={{height : '90vh', display : 'flex', alignItems : 'center', justifyContent : 'center', overflow : 'hidden'}}>
+      <Container style={{height : '70vh', display : 'flex', alignItems : 'center', justifyContent : 'center', overflow : 'hidden'}}>
         <img src={hero1} alt="" className='h-25 shadow-lg mx-2 ' style={{border : 'none', borderRadius : '15px'}}></img>
         <img src={hero} alt="" className='shadow-lg' style={{border : 'none', borderRadius : '15px', maxWidth : '90%', maxHeight : '75%'}}></img>
         <img src={hero1} alt="" className='h-25 shadow-lg mx-2 ' style={{border : 'none', borderRadius : '15px'}}></img>
