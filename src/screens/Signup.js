6d722e3f83b3468e3e50c78ce150961ccc37056b
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-06 11:51:56 +0100

    updated error message in signup page

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -74,7 +74,7 @@ export default function SignIn() {
                 type="text"
                 placeholder="Last name"
               />
-              <Form.Control.Feedback type="invalid">Please provide your last email.</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">Please provide your last name.</Form.Control.Feedback>
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
             <Form.Group as={Col} md="12" controlId="validationCustomUsername">
