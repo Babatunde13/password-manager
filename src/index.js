3716c51a696fe6753f85fb954b31cbed72d62322
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-06 13:36:03 +0100

    Added PWA feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -9,4 +9,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+serviceWorkerRegistration.register();
